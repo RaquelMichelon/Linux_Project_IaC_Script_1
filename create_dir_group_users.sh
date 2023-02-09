5258a708f61aab3d3026bd570f5f9765c1838599
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2023-02-09 15:30:39 +0000

    Update create_dir_group_users.sh

--- create_dir_group_users.sh
+++ create_dir_group_users.sh
@@ -13,29 +13,29 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Creating users..."
+echo "Creating users and inserting them into groups..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_ADM
-useradd sarah -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_ADM
-useradd regina -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_ADM
+useradd carlos -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_ADM
+useradd maria -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_ADM
+useradd joao -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_ADM
 
-useradd maria -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SALES
-useradd joao -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SALES
-useradd eduardo -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SALES
+useradd debora -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_VEN
+useradd serbastiana -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_VEN
+useradd roberto -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_VEN
 
-useradd mara -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SECURITY
-useradd luzia -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SECURITY
-useradd valmir -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SECURITY
+useradd josefina -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SEC
+useradd amanda -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SEC
+useradd rogerio -c "Invited User" -m -s /bin/bash -p $(openssl passwd Pass123) -G GRP_SEC
 
-echo "Specifing directories permissions..."
+echo "Specifing group's directories and permissions..."
 
 chown root:GRP_ADM /adm
-chown root:GRP_SALES /sales
-chown root:GRP_SECURITY /security
+chown root:GRP_SALES /ven
+chown root:GRP_SECURITY /sec
 
 chmod 770 /adm
-chmod 770 /sales
-chmod 770 /security
-chmod 777 /public
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
 
 echo "Ending..."
